ad7b6e57454f1fb5d87a8665f946475f487ac3e2
Codingvibes8 <wecodeappz@gmail.com>
2025-02-13 12:14:39 +0000

    app page changes

--- src/app/page.js
+++ src/app/page.js
@@ -2,12 +2,12 @@ import Link from 'next/link';
 
 export default function Home() {
   return (
-    <>
-      <div className='flex flex-col min-h-[60vh] w-full px-3   bg-[home-background]'>
+    <div>
+      <div className='flex flex-col min-h-[60vh] w-full px-3 bg-cover bg-center' style={{'home-img': "url('/bcg.jpg')" }}>
         <h1 className='text-center text-3xl font-bold lg:text-6xl px-12 pt-16 mb-10'>
           Welcome to Developers Blog
         </h1>
-        <p className='text-gray-200 text-lg md:text-2xl max-w-2xl mx-auto px-12'>
+        <p className='text-gray-200 md:text-2xl max-w-2xl mx-auto px-12'>
           Discover a variety of articles and tutorials on topics such as web
           development, software engineering, and programming languages, all
           brought to you through a blog built with Next.js and{' '}
@@ -32,12 +32,12 @@ export default function Home() {
       </div>
       <div className='p-3 flex flex-col gap-8 py-7'>
         <Link
-          href={'/'}
+          href='/'
           className='text-lg text-gray-800 hover:underline text-center'
         >
           View all posts
         </Link>
       </div>
-    </>
+    </div>
   );
 }
